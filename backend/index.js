5d9a1af2902e6c9807ecc95ebb5fcc935836a37f
RajeshVerma1612 <verma.rajesh123v@gmail.com>
2024-07-02 15:27:33 +0530

    update user api route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ dotenv.config();
 const app=express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000!");
